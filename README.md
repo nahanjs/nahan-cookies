ace012b18bf121509addbcf0f0efd2be899dffe5
LabMemNo003 <LabMemNo003@gmail.com>
2020-06-24 20:18:36 +0800

    Untested implementation

--- README.md
+++ README.md
@@ -5,3 +5,26 @@ Cookies middleware for nahan
 ## Reference
 
 https://tools.ietf.org/html/rfc6265
+https://tools.ietf.org/html/draft-ietf-httpbis-rfc6265bis-05
+
+## Other
+
+Due to the following code in `node-v12.18.0/lib/_http_outgoing.js`, when setting multiple `Set-cookie` headers for http response, the available type of `value` parameter of `response.setHeader(name, value)` is restricted to `Array`. Passing an `iterable` or `generator` typed `value` parameter will give a unexpected header.
+
+``` javascript
+function processHeader(self, state, key, value, validate) {
+  if (validate)
+    validateHeaderName(key);
+  if (ArrayIsArray(value)) {
+    if (value.length < 2 || !isCookieField(key)) {
+      // Retain for(;;) loop for performance reasons
+      // Refs: https://github.com/nodejs/node/pull/30958
+      for (let i = 0; i < value.length; i++)
+        storeHeader(self, state, key, value[i], validate);
+      return;
+    }
+    value = value.join('; ');
+  }
+  storeHeader(self, state, key, value, validate);
+}
+```
--- index.js
+++ index.js
@@ -5,21 +5,74 @@ module.exports = Cookies;
 function Cookies() {
 
     return async (ctx, next) => {
-        init_cookies(ctx);
+        if (ctx.cookies === undefined)
+            ctx.cookies = new _Cookies(ctx.req, ctx.res);
+
         await next();
     };
 }
 
-function init_cookies(ctx) {
-    if (ctx.cookies !== undefined)
-        return;
+class _Cookies {
+    constructor(req, res) {
+        this.req = req;
+        this.res = res;
 
-    const header = ctx.req.headers['cookie'];
-    if (header === undefined)
-        return;
+        this.parsed = undefined;
 
-    const cookies = header.split(';');
-    for (const cookie of cookies){
-        console.log(cookie);
+        this.headers = [];
+        this.res.setHeader('Set-Cookie', this.headers);
+    }
+
+    get(key) {
+        if (this.parsed === undefined) {
+            this.parsed = {};
+
+            const header = this.req.headers['cookie'];
+            if (header !== undefined) {
+
+                const cookies = header.split(';');
+                for (const cookie of cookies) {
+                    const pos = cookie.indexOf('=');
+                    const key = cookie.slice(0, pos);
+                    const val = cookie.slice(pos + 1);
+
+                    this.parsed[key] = val;
+                }
+            }
+        }
+
+        return this.parsed[key];
+    }
+
+    set(key, val, attrs = default_attrs) {
+        if (this.res.headersSent)
+            throw new Error();
+        this.headers.push(new _Cookie(key, val, attrs));
     }
 }
+
+class _Cookie {
+    constructor(key, val, attrs = default_attrs) {
+        this.key = key;
+        this.val = val;
+        this.attrs = attrs;
+    }
+
+    toString() {
+        let header = this.key + '=' + this.val;
+
+        const attrs = this.attrs;
+        if (attrs.expires) header += '; expires=' + attrs.expires;
+        if (attrs.max_age) header += '; max-age=' + attrs.max_age;
+        if (attrs.domain) header += '; domain=' + attrs.domain;
+        if (attrs.path) header += '; path=' + attrs.path;
+        if (attrs.secure) header += '; secure';
+        if (attrs.httponly) header += '; httponly';
+    }
+}
+
+var default_attrs = {
+    expires: undefined, max_age: undefined,
+    domain: undefined, path: undefined,
+    secure: false, httponly: false,
+};
